1c33a939f25420ade84fa2c4bbfcc57f7ea2b20e
pradipwasre <pradipwasre20@gmail.com>
2023-01-13 22:31:32 +0530

    updating

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+
+
 
 if __name__ == '__main__':
     mongodb_client = MongoDBClient()
